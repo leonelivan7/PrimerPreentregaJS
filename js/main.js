e61420525f788c55bc4912a27c8e18fc0d2fe10c
Iván Leonel Ríos <118583649+leonelivan7@users.noreply.github.com>
2024-08-26 00:54:17 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,48 +1,51 @@
 function cotizacion() {
-    let pregunta = prompt("¡Hola, bienvendo! Con que moneda desea operar? \n 1)Dolar \n 2)Euro \n 3)Real");
+    let pregunta = prompt("¡Hola, bienvenido! Con que moneda desea operar? \n 1)Dolar \n 2)Euro \n 3)Real");
     if (pregunta == 1) {
-            let pregunta2 = prompt("Que cotización queres saber? \n 1)Compra \n 2)Venta");
+            let pregunta2 = prompt("Que cotización quiere saber? \n 1)Compra \n 2)Venta");
             if (pregunta2 == 1) {
-                alert("La cotización del dolar es 1350");
+                alert("La cotización del dolar para la compra es 1350");
             } else if (pregunta2 == 2) {
-                alert("La cotización del dolar es 1330");
+                alert("La cotización del dolar para la venta es 1330");
             } else {
                 alert("Operación invalida");
             }
-            let pregunta3 = confirm("Queres seguir operando?");
+            let pregunta3 = confirm("Quiere seguir operando?");
             if (pregunta3 === true) {
                 cotizacion();
-            } else if {
-                alert("Gracias por operar con nosotros!)")
+            } else {
+                alert("¡Gracias por operar con nosotros!");
+            }
         }
     else if (pregunta == 2){
-            let pregunta2 = prompt("Que cotización queres saber? \n 1)Compra \n 2)Venta");
+            let pregunta2 = prompt("Que cotización quiere saber? \n 1)Compra \n 2)Venta");
             if (pregunta2 == 1) {
-                alert("La cotización del euro es 1500");
+                alert("La cotización del euro para la compra es 1500");
             } else if (pregunta2 == 2) {
-                alert("La cotización del euro es 1600");
+                alert("La cotización del euro para la venta es 1600");
             } else {
                 alert("Operación invalida");
             }
-            let pregunta3 = confirm("Queres seguir operando?");
+            let pregunta3 = confirm("Quiere seguir operando?");
             if (pregunta3 === true) {
                 cotizacion();
-            } else if {
-                alert("Gracias por operar con nosotros!)")
+            } else {
+                alert("Gracias por operar con nosotros");
+            }
     } else if (pregunta == 3){
-            let pregunta2 = prompt("Que cotización queres saber? \n 1)Compra \n 2)Venta");
+            let pregunta2 = prompt("Que cotización quiere saber? \n 1)Compra \n 2)Venta");
             if (pregunta2 == 1) {
-                alert("La cotización del real es 200");
+                alert("La cotización del real para la compra es 200");
             } else if (pregunta2 == 2) {
-                alert("La cotización del real es 270");
+                alert("La cotización del real para la venta es 270");
             } else {
                 alert("Operación invalida");
             }
-            let pregunta3 = confirm("Queres seguir operando?");
+            let pregunta3 = confirm("Quiere seguir operando?");
             if (pregunta3 === true) {
                 cotizacion();
-            } else if {
-                alert("Gracias por operar con nosotros!)")
+            } else {
+                alert("Gracias por operar con nosotros");
+            }
     } else {
         alert("Operación invalida");
     }
